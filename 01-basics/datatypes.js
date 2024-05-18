5409a77130b2e0ecbaac16df592d9f05c9bbd8a0
Muhammad Umair Ali <92013504+bossthekhani@users.noreply.github.com>
2024-05-18 14:04:29 +0000

    git commit -m "datatypes"

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,22 @@
+"user strict"; //treat all js code as newer version
+// alert(3 + 3); we are using node.js not browser 
+
+let name = "umair"
+let age = "20"
+let isLogin = false
+let state;
+/*
+Primitive DataType
+*/
+//number => 2 power of 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniques
+
+
+// Object's 
+console.log(typeof null); //object
+console.log(typeof undefined); //undefine 
---
+++ 01-basics/var.js
@@ -0,0 +1,11 @@
+const accountId = 14455
+let accountemail = "umair@gmail.com"
+let accountPass = "12345"
+accountCity = "Pesh"
+let accountState;
+
+console.log(accountId);
+ accountemail = "hc@hc@gmail.com"
+ accountPass = "23456"
+ accountCity = "MDN"
+ console.table([accountId, accountPass, accountemail, accountCity, accountState]);
