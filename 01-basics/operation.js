8e1792c6bbaec32e379bc148f50b3f753562bd41
Muhammad Umair Ali <92013504+bossthekhani@users.noreply.github.com>
2024-05-19 08:06:27 +0000

    operation in JS

---
+++ 01-basics/operation.js
@@ -0,0 +1,24 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " umair"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 +3);
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+2);
+// console.log(+"");
+
+let counter = 100
+counter++;
+console.log(counter);
