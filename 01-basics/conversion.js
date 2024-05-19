c6ac86e98848f89aa53a34ddf7a95661de2085e1
Muhammad Umair Ali <92013504+bossthekhani@users.noreply.github.com>
2024-05-19 07:37:12 +0000

    conversion in jS

---
+++ 01-basics/conversion.js
@@ -0,0 +1,26 @@
+let score = "Umair"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//c//onsole.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN (Not a Number)
+// ture => 1; false => 0
+// string => NAN
+
+let isLogin = "jaldl"
+let booleanislogin = Boolean (isLogin)
+//console.log(booleanislogin);
+
+// 1 => true; 
+// "" => false
+// 0 => false
+// "umair" => true
+
+let someNumber = 40
+let stringno = String (someNumber)
+console.log(stringno);
