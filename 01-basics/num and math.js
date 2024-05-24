f5fde1d87a8fd5e33464336e8bb4419ebca95530
Muhammad Umair Ali <92013504+bossthekhani@users.noreply.github.com>
2024-05-24 19:53:48 +0500

    Create num and math.js

---
+++ 01-basics/num and math.js
@@ -0,0 +1,39 @@
+const score = 400;
+//  console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernum = 123.8038;
+// console.log(othernum.toPrecision(4));
+
+const hundred = 1000000;
+// //console.log(hundred.toLocaleString('en-IN'));
+// //console.log(hundred.toLocaleString('en-US'));
+// //console.log(hundred.toLocaleString('en-GB'));
+// //console.log(hundred.toLocaleString('ar-SA'));
+// //console.log(hundred.toLocaleString('ar-EG'));
+// //console.log(hundred.toLocaleString('fa-IR'));
+// //console.log(hundred.toLocaleString('de-DE'));
+// //console.log(hundred.toLocaleString('fr-FR'));
+
+//+++++++++ Mathematics and functions +++++++++
+// console.log(Math);
+// // console.log(Math.random(4.6));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.6));
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(-10));
+// console.log(Math.max(10, 20, 30, 40, 50));
+// console.log(Math.min(10, 20, 30, 40, 50));
+// console.log(Math.max(10, 20));
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.random() * 10 + 2);
+
+const max = 10;
+const min = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
